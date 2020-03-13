f042901ca4bacf4add0db4fb858cf161e2efe70e
Karim El Shami <karim.elshami@vezeeta.com>
2020-03-13 19:24:31 +0200

    intial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MainContainer from 'modules/common/containers/MainContainer'
+ReactDOM.render(<MainContainer />, document.getElementById('root'))
